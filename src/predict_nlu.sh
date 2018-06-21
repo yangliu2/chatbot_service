e1cdf81750130cd3207ab24370b888bf4be59b64
Yang Liu <yangliu3456@gmail.com>
2018-06-20 20:54:03 -0400

    init

---
+++ src/predict_nlu.sh
@@ -0,0 +1,1 @@
+curl -X POST localhost:5000/parse -d '{"q":"shaming"}' | python -m json.tool
---
+++ src/train_nlu.sh
@@ -0,0 +1,4 @@
+python -m rasa_nlu.train \
+    --config data/config_spacy.yml \
+    --data data/intents.json \
+    --path projects/yang
